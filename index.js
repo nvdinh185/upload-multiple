48db582da9c8a4c42756f1f6ea5b4750c6676900
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-31 07:27:17 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+
+var dirUpload = 'uploads';
+if (!fs.existsSync(dirUpload)) fs.mkdirSync(dirUpload);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, dirUpload);
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+var upload = multer({ storage: storage });
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', upload.array('multi-files'), (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(3000);
